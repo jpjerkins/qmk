bf441ac2bd9f3ce9202fbb414b743c18a4fc6208
Phil Jerkins <Phil@TheJerkins.net>
2021-04-26 21:40:05 -0500

    feat: Implemented desired keymap

--- keyboards/gboards/k/gergoplex/keymaps/phil/keymap.c
+++ keyboards/gboards/k/gergoplex/keymaps/phil/keymap.c
@@ -10,8 +10,11 @@
 #include "g/keymap_combo.h"
 
 #define BASE 0 // default layer
-#define SYMB 1 // symbols
-#define NUMB 2 // numbers/motion
+#define SYMB 1 // symbols/nav
+#define NPAD 2 // number pad
+#define NFN 3 // numbers/Fn keys
+#define NAV2 4 // nav 2
+#define BMOD 5 // base mods
 
 /* Combomap
  *
@@ -29,61 +32,51 @@
 
 // Blank template at the bottom
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-/* Keymap 0: Basic layer
- *
- * ,-----------------------------.       ,--------------------------------.
- * |    Q |  W  |  E  |  R  |  T  |      |  Y  |  U  |  I  |  O  |    P   |
- * |-----+-----+-----+-----+------|      |--------------------------------|
- * |CTRL/A|  S  |  D  |  F  |  G  |      |  H  |  J  |  K  |  L  | CTRL/; | 
- * |-----+-----+-----+-----+------+      |--------------------------------|
- * |SHFT/Z|  X  |  C  |  V  |  B  |      |  N  |  M  |  <  |  >  | SHFT/? |
- * `------+-----+-----+------+----'      `--------------------------------'
- *  .-------------------------.           .-----------------.   
- *  |ESC/META|ENT/ALT|SPC(SYM)|           |SPC(NUM)|BSPC|TAB|
- *  '-------------------------'           '-----------------' 
- */
+// Keymap 0: Basic layer
 [BASE] = LAYOUT_gergoplex(
-    KC_Q,                        KC_W, KC_E, KC_R, KC_T,    KC_Y, KC_U, KC_I,    KC_O,   KC_P, 
-    MT(MOD_LCTL, KC_A),KC_S, KC_D, KC_F, KC_G,      KC_H, KC_J, KC_K,    KC_L,   MT(MOD_LCTL, KC_SCLN),
-    MT(MOD_RSFT, KC_Z),KC_X, KC_C, KC_V, KC_B,      KC_N, KC_M, KC_COMM, KC_DOT, MT(MOD_RSFT, KC_SLSH),
-
-        MT(MOD_LGUI, KC_ESC), MT(MOD_LALT, KC_ENT), LT(SYMB, KC_SPC),                                   // Left
-        LT(NUMB, KC_SPC), KC_LSFT, MT(MOD_RSFT, KC_TAB)                                                             // Right
+            KC_Q, KC_W, KC_E, KC_R, KC_T,            KC_Y, KC_U, KC_I, KC_O, KC_P,
+            KC_A, KC_S, KC_D, KC_F, KC_G,            KC_H, KC_J, KC_K, KC_L, KC_SCLN,
+            KC_Z, KC_X, KC_C, KC_V, KC_B,            KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,
+            MO(2), KC_LSFT, MO(3),            MO(1), KC_SPC, MO(5)
     ),
-/* Keymap 1: Symbols layer
- * ,-----------------------------.       ,--------------------------------.
- * |  !   |  @  |  {  |  }  |     |      |  ~  |     |     |    \   |
- * |-----+-----+-----+-----+------|      |--------------------------------|
- * |  #   |  $  |  (  |  )  | LMB |      |  +  |  -  |  /  |  *  |    '   | 
- * |-----+-----+-----+-----+------+      |--------------------------------|
- * |  %   |  ^  |  [  |  ]  | RMB |      |  &  |  =  |  ,  |  .  |   -    |
- * `------+-----+-----+------+----'      `--------------------------------'
- *              .-----------------.      .------------------.   
- *              |MMB |  ;   |  =  |      |  =  |  ;  |  DEL |
- *              '-----------------'      '------------------' 
- */
+// Keymap 1: Symbols/Nav layer
 [SYMB] = LAYOUT_gergoplex(
-    KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE,   KC_GRV,  KC_TILD, KC_TRNS, KC_TRNS, KC_BSLS,
-    KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_BTN2,   KC_PLUS, KC_MINS, KC_SLSH, KC_ASTR, KC_QUOT,
-    KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_BTN1,   KC_AMPR, KC_EQL,  KC_COMM, KC_DOT,  KC_MINS,
-                      CMB_TOG, KC_SCLN, KC_EQL,   KC_EQL,  KC_SCLN, KC_DEL
+            KC_TAB, KC_QUOT, KC_LPRN, KC_RPRN, KC_PSCR,            KC_TRNS, KC_VOLU, KC_VOLD, KC_TRNS, KC_ESC,
+            KC_MINS, KC_EQL, KC_LCBR, KC_RCBR, KC_ASTR,            KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_TRNS,
+            MT(MOD_LCTL, KC_GRV), MT(MOD_LSFT, KC_BSLS), MT(MOD_LGUI, KC_LBRC), MT(MOD_LALT, KC_RBRC), KC_TRNS,
+            KC_BSPC, MT(MOD_RALT, KC_BSPC), MT(MOD_RGUI, KC_BSPC), KC_RSFT, KC_RCTL,
+            MO(4), KC_TRNS, KC_TRNS,
+            KC_TRNS, KC_TRNS, KC_TRNS
     ),
-/* Keymap 2: Pad/Function layer
- * ,-----------------------------.       ,-------------------------------.
- * |  1   |  2  |  3  |  4  |  5  |      |  6  |  7  |  8  |  9  |   0   |
- * |-----+-----+-----+-----+------|      |-------------------------------|
- * |  F1  | F2  | F3  | F4  |  F5 |      | LFT | DWN | UP  | RGT | VOLUP | 
- * |-----+-----+-----+-----+------+        |-------------------------------|
- * |  F6  | F7  | F8  | F9  | F10 |      |MLFT | MDWN| MUP | MRGT| VOLDN |
- * `------+-----+-----+------+----'        `-------------------------------'
- *                  .-----------------.           .-----------------.   
- *                  | F11 | F12|        |                       |     | PLY | SKP |
- *                  '-----------------'           '-----------------' 
- */
-[NUMB] = LAYOUT_gergoplex(
-    KC_1,  KC_2,  KC_3,  KC_4,  KC_5,               KC_6,   KC_7,    KC_8,      KC_9,    KC_0,
-    KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_VOLU,
-    KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,         KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_VOLD,
-                              KC_F11,KC_F12,KC_TRNS,    KC_TRNS,KC_MPLY,KC_MNXT
+// Keymap 2: Number Pad layer
+[NPAD]  LAYOUT_gergoplex(
+            KC_WH_U, KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1,            KC_NLCK, KC_P7, KC_P8, KC_P9, KC_PMNS,
+            KC_WH_D, KC_MS_L, KC_MS_U, KC_MS_D, KC_MS_R,            KC_MS_U, KC_P4, KC_P5, KC_P6, KC_PPLS,
+            KC_LCTL, KC_LSFT, KC_LGUI, KC_LALT, KC_BTN2,            KC_MS_D, KC_P1, KC_P2, KC_P3, KC_PPLS,
+            KC_TRNS, KC_RALT, KC_TRNS,            KC_P0, KC_PDOT, KC_PENT
+    ),
+[NFN] = LAYOUT_gergoplex(
+            KC_1, KC_2, KC_3, KC_4, KC_5,            KC_6, KC_7, KC_8, KC_9, KC_0,
+            KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,            KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,
+            MT(MOD_LCTL, KC_F11), MT(MOD_LSFT, KC_F12), KC_LGUI, KC_LALT, KC_TRNS,
+            KC_TRNS, KC_RALT, KC_RGUI, KC_RSFT, KC_RCTL,
+            KC_TRNS, KC_TRNS, KC_TRNS,
+            KC_TRNS, KC_TRNS, KC_TRNS
+    ),
+// Keymap 1: Symbols/Nav layer
+[NAV2] = LAYOUT_gergoplex(
+            KC_RALT, KC_RALT, KC_TRNS, KC_TRNS, KC_TRNS,            KC_TRNS, KC_INS, KC_HOME, KC_PGUP, KC_TRNS,
+            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,            KC_TRNS, KC_DEL, KC_END, KC_PGDN, KC_TRNS,
+            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,            KC_TRNS, KC_RALT, KC_RGUI, KC_RSFT, KC_RCTL,
+            KC_TRNS, KC_TRNS, KC_TRNS,            KC_TRNS, KC_TRNS, KC_TRNS
+    ),
+// Keymap 2: Number Pad layer
+[BMOD]  LAYOUT_gergoplex(
+            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+            MT(MOD_LCTL, KC_Z), MT(MOD_LSFT, KC_X), MT(MOD_LGUI, KC_C), MT(MOD_LALT, KC_V), KC_TRNS,
+            KC_TRNS, MT(MOD_RALT, KC_M), MT(MOD_RGUI, KC_COMM), MT(MOD_RSFT, KC_DOT), MT(MOD_RCTL, KC_SLSH),
+            KC_TRNS, KC_TRNS, KC_TRNS,
+            KC_TRNS, KC_TRNS, KC_TRNS
     )
 };
