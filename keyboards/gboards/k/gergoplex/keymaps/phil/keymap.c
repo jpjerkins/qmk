635e0c02af3de6c7562abeacb3b28bc59e2eef06
Phil Jerkins <Phil@TheJerkins.net>
2021-04-29 22:39:53 -0500

    fix: Switched to the correct set of modifier keycodes for the ModTap plugin

--- keyboards/gboards/k/gergoplex/keymaps/phil/keymap.c
+++ keyboards/gboards/k/gergoplex/keymaps/phil/keymap.c
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [NPAD]  LAYOUT_gergoplex(
             KC_TRNS, KC_WH_L, KC_WH_U, KC_WH_D, KC_WH_R,            KC_NLCK, KC_P7, KC_P8, KC_P9, KC_PMNS,
             KC_TRNS, KC_MS_L, KC_MS_U, KC_MS_D, KC_MS_R,            KC_MS_U, KC_P4, KC_P5, KC_P6, KC_PPLS,
-            KC_LCTL, MT(KC_LSFT, KC_BTN3), MT(KC_LGUI, KC_BTN2), MT(KC_LALT, KC_BTN1), KC_TRNS,
+            KC_LCTL, MT(MOD_LSFT, KC_BTN3), MT(MOD_LGUI, KC_BTN2), MT(MOD_LALT, KC_BTN1), KC_TRNS,
             KC_MS_D, KC_P1, KC_P2, KC_P3, KC_PPLS,
             KC_TRNS, KC_RALT, KC_TRNS,            KC_P0, KC_PDOT, KC_PENT
     ),
