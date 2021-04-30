db579e4e5d00c089a3079994017dad29aa7612e0
Phil Jerkins <Phil@TheJerkins.net>
2021-04-29 22:46:57 -0500

    chore: Switched to abbreviated ModTap macros for brevity

--- keyboards/gboards/k/gergoplex/keymaps/phil/keymap.c
+++ keyboards/gboards/k/gergoplex/keymaps/phil/keymap.c
@@ -43,8 +43,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [SYMB] = LAYOUT_gergoplex(
             KC_TAB, KC_QUOT, KC_LPRN, KC_RPRN, KC_PSCR,            KC_TRNS, KC_VOLU, KC_VOLD, KC_TRNS, KC_ESC,
             KC_MINS, KC_EQL, KC_LCBR, KC_RCBR, KC_ASTR,            KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_TRNS,
-            MT(MOD_LCTL, KC_GRV), MT(MOD_LSFT, KC_BSLS), MT(MOD_LGUI, KC_LBRC), MT(MOD_LALT, KC_RBRC), KC_TRNS,
-            KC_BSPC, MT(MOD_RALT, KC_BSPC), MT(MOD_RGUI, KC_BSPC), KC_RSFT, KC_RCTL,
+            LCTL_T(KC_GRV), LSFT_T(KC_BSLS), LGUI_T(KC_LBRC), LALT_T(KC_RBRC), KC_TRNS,
+            KC_BSPC, RALT_T(KC_BSPC), RGUI_T(KC_BSPC), KC_RSFT, KC_RCTL,
             MO(4), KC_TRNS, KC_ENT,
             KC_TRNS, KC_TRNS, KC_TRNS
     ),
@@ -52,14 +52,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [NPAD]  LAYOUT_gergoplex(
             KC_TRNS, KC_WH_L, KC_WH_U, KC_WH_D, KC_WH_R,            KC_NLCK, KC_P7, KC_P8, KC_P9, KC_PMNS,
             KC_TRNS, KC_MS_L, KC_MS_U, KC_MS_D, KC_MS_R,            KC_MS_U, KC_P4, KC_P5, KC_P6, KC_PPLS,
-            KC_LCTL, MT(MOD_LSFT, KC_BTN3), MT(MOD_LGUI, KC_BTN2), MT(MOD_LALT, KC_BTN1), KC_TRNS,
+            KC_LCTL, LSFT_T(KC_BTN3), LGUI_T(KC_BTN2), LALT_T(KC_BTN1), KC_TRNS,
             KC_MS_D, KC_P1, KC_P2, KC_P3, KC_PPLS,
             KC_TRNS, KC_RALT, KC_TRNS,            KC_P0, KC_PDOT, KC_PENT
     ),
 [NFN] = LAYOUT_gergoplex(
             KC_1, KC_2, KC_3, KC_4, KC_5,            KC_6, KC_7, KC_8, KC_9, KC_0,
             KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,            KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,
-            MT(MOD_LCTL, KC_F11), MT(MOD_LSFT, KC_F12), KC_LGUI, KC_LALT, KC_TRNS,
+            LCTL_T(KC_F11), LSFT_T(KC_F12), KC_LGUI, KC_LALT, KC_TRNS,
             KC_TRNS, KC_RALT, KC_RGUI, KC_RSFT, KC_RCTL,
             KC_TRNS, KC_TRNS, KC_TRNS,
             KC_TRNS, KC_TRNS, KC_TRNS
@@ -75,8 +75,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BMOD]  LAYOUT_gergoplex(
             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-            MT(MOD_LCTL, KC_Z), MT(MOD_LSFT, KC_X), MT(MOD_LGUI, KC_C), MT(MOD_LALT, KC_V), KC_TRNS,
-            KC_TRNS, MT(MOD_RALT, KC_M), MT(MOD_RGUI, KC_COMM), MT(MOD_RSFT, KC_DOT), MT(MOD_RCTL, KC_SLSH),
+            LCTL_T(KC_Z), LSFT_T(KC_X), LGUI_T(KC_C), LALT_T(KC_V), KC_TRNS,
+            KC_TRNS, RALT_T(KC_M), RGUI_T(KC_COMM), RSFT_T(KC_DOT), RCTL_T(KC_SLSH),
             KC_TRNS, KC_TRNS, KC_TRNS,
             KC_TRNS, KC_TRNS, KC_TRNS
     )
